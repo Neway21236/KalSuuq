a3e1136957f8a090aab60e0c4889d59281d9d6de
Neway21236 <newaymr@gmail.com>
2026-05-07 11:31:48 +0300

    kalsuuq first commit1

--- src/app/api/admin/orders/[id]/cancel/route.ts
+++ src/app/api/admin/orders/[id]/cancel/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/db";
 import { Prisma } from "@prisma/client";
-import { logger } from "@/lib/logger";
 
 export async function POST(req: NextRequest, { params }: { params: { id: string } }) {
   try {
